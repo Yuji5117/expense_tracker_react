86e162f44095300b1141f0f79c7e14de3e20fba5
Yuji Nakamura <ny5117@icloud.com>
2022-03-26 07:55:20 +0900

    transactionとbalanceを動的に表示する様に修正

--- src/components/Transaction.tsx
+++ src/components/Transaction.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { TransactionType } from "../context/GlobalState";
 
-const Transaction = ({transaction}: {transaction: TransactionType}) => {
+const Transaction = ({ transaction }: { transaction: TransactionType }) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
   return (
     <div>
-      <li className="minus">
-        {transaction.text} <span>-$400</span>
+      <li className={transaction.amount < 0 ? "minus" : "plus"}>
+        {transaction.text}{" "}
+        <span>
+          {sign}${Math.abs(transaction.amount)}
+        </span>
         <button className="delete-btn">x</button>
       </li>
     </div>
